2817b170a98a7e8aab57013cac89984f4a4d062e
PhuocHoang <htphuoc1995@gmail.com>
2023-12-07 23:46:23 -0500

    Chapter 13 - Scrip5ng the controller for jumping and wall sliding

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 typeof(Animator))]
 public class SoyBoyController : MonoBehaviour
 {
+    public float jumpDurationThreshold = 0.25f;
+    private float jumpDuration;
     public float speed = 14f;
     public float accel = 6f;
     private Vector2 input;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     private Animator animator;
+    public bool isJumping;
+    public float jumpSpeed = 8f;
+    private float rayCastLengthCheck = 0.005f;
+    private float width;
+    private float height;
     void Start()
     {
         
+    }
+    public bool PlayerIsOnGround()
+    {
+        // 1
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(
+                transform.position.x, transform.position.y - height),
+            -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(
+                transform.position.x + (width - 0.2f),
+                transform.position.y - height), -Vector2.up,
+            rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(
+                transform.position.x - (width - 0.2f),
+                transform.position.y - height), -Vector2.up,
+            rayCastLengthCheck);
+        // 2
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
     void Update()
     {
@@ -35,6 +68,23 @@ public class SoyBoyController : MonoBehaviour
         {
             sr.flipX = true;
         }
+        if (input.y >= 1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+        if (jumpDuration > jumpDurationThreshold) input.y = 0f;
     }
     void FixedUpdate()
     {
@@ -55,5 +105,9 @@ public class SoyBoyController : MonoBehaviour
                                 * acceleration, 0));
         // 4
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 }
